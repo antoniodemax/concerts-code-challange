8f8c5e0b883f0f32bd5a7c6a7d37954fcf76ffcd
Anthony <anthony.onyango@student.moringaschool.com>
2024-09-14 18:20:35 +0300

    fixed the database connection issue by connecting to the correct database file in the execute_main() function.

--- concert_database_queries.py
+++ concert_database_queries.py
@@ -1,49 +1,5 @@
 import sqlite3
 
-# Connecting to the database
-conn = sqlite3.connect('concerts.db')
-cursor = conn.cursor()  
-
-# Bands table
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS bands (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    hometown TEXT NOT NULL
-)
-''')
-
-# Venues table
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS venues (
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    city TEXT NOT NULL
-)
-''')
-
-# Concerts table
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS concerts (
-    id INTEGER PRIMARY KEY,
-    band_id INTEGER,
-    venue_id INTEGER,
-    date TEXT NOT NULL,
-    FOREIGN KEY (band_id) REFERENCES bands (id),
-    FOREIGN KEY (venue_id) REFERENCES venues (id)
-)
-''')
-
-# Insert data into concerts table
-cursor.execute('''
-INSERT INTO concerts (band_id, venue_id, date) 
-VALUES (1, 1, '2023-10-15')
-''')
-
-# Committing the changes and closing the connection
-conn.commit()
-conn.close()
-
 # fetch a band by concert ID
 def fetch_band_by_concert(concert_id, db_cursor):
     db_cursor.execute('''
