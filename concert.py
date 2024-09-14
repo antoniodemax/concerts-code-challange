8336c7990aa6dac5b2873b362a5df0e4288c385f
Anthony <anthony.onyango@student.moringaschool.com>
2024-09-14 21:11:08 +0300

    dropped the table and created again

--- concert.py
+++ concert.py
@@ -1,18 +1,79 @@
+import sqlite3
+
+class Venue:
+    def __init__(self, db_name):
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+
+    def concert_on(self, date):
+        query = "SELECT * FROM concerts WHERE date = ? ORDER BY time LIMIT 1"
+        self.cursor.execute(query, (date,))
+        return self.cursor.fetchone()
+
+    def most_frequent_band(self):
+        query = """
+        SELECT band_name, COUNT(*) as performance_count 
+        FROM concerts 
+        GROUP BY band_name 
+        ORDER BY performance_count DESC 
+        LIMIT 1
+        """
+        self.cursor.execute(query)
+        return self.cursor.fetchone()
+
+    def close(self):
+        self.connection.close()
+
+
 class Concert:
-    def __init__(self, band_name, band_hometown, venue_city):
-        self.band_name = band_name
-        self.band_hometown = band_hometown
-        self.venue_city = venue_city
+    def __init__(self, name, venue):
+        self.name = name
+        self.venue = venue
 
-    def hometown_show(self):
-        #checks if the concert is in the bands hometown
-        return self.band_hometown.lower() == self.venue_city.lower()
 
-    def introduction(self):
-        return f"Hello {self.venue_city}!!!!! We are {self.band_name} and we're from {self.band_hometown}"  
+class Band:
+    def __init__(self, name):
+        self.name = name
+        self.concerts_list = []
 
-#example
-concert = Concert("The Beatles", "London", "New York")
-print(concert.introduction())
-print(concert.hometown_show())     
-        
+    def add_concert(self, concert):
+        self.concerts_list.append(concert)
+
+    def concerts(self):
+        return [concert.name for concert in self.concerts_list]
+
+    def venues(self):
+        return list(set(concert.venue for concert in self.concerts_list))
+
+
+# Connect to the database
+conn = sqlite3.connect('concerts.db')
+cursor = conn.cursor()
+
+cursor.execute('DROP TABLE IF EXISTS concerts')
+
+# Create the concerts table with the correct schema
+cursor.execute('''
+    CREATE TABLE concerts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        venue TEXT NOT NULL
+    )
+''')
+
+# Create a band and add concerts
+band = Band("The Rockers")
+band.add_concert(Concert("Jazz session", "Nairobi"))
+band.add_concert(Concert("Winter Jam", "Mombasa"))
+band.add_concert(Concert("Ladies Night", "Kisumu"))
+
+# Insert concert data into the database
+for concert in band.concerts_list:
+    cursor.execute("INSERT INTO concerts (name, venue) VALUES (?, ?)",
+                   (concert.name, concert.venue))
+
+# Commit the changes
+conn.commit()
+
+# Closing the connection
+conn.close()
